e26b4eed38fe9a279ba77d8f8967f52b7a28d5fd
Marvin <marvin@reachdigital.media>
2025-01-24 16:12:03 +0000

    quote single slider
    Quote single slider

---
+++ wp-content/themes/reach/assets/components/single-quote-block/functions.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Reach\Components\SingleQuoteBlock;
+
+function filterArgs(array $args): ?array
+{
+    // -------------------------------------------------------------------------
+    // Default arguments.
+    // -------------------------------------------------------------------------
+    $args = array_merge([
+        'classes' => [],
+        'heading' => 'Hello World',
+    ], $args);
+
+    // -------------------------------------------------------------------------
+    // Required classes.
+    // -------------------------------------------------------------------------
+    $args['classes'] = array_merge([
+        'single-quote',
+        'wp-block',
+    ], $args['classes']);
+
+    if (!empty($args['heading'])) {
+        $args['heading'] = [
+            'heading' => $args['heading'],
+            'classes' => ['single-quote_heading'],
+        ];
+    }
+
+    // -------------------------------------------------------------------------
+    // Return the filtered args.
+    // -------------------------------------------------------------------------
+    return $args;
+}
---
+++ wp-content/themes/reach/assets/components/single-quote-block/hooks.php
@@ -0,0 +1,5 @@
+<?php
+
+namespace Reach\Components\SingleQuoteBlock;
+
+\add_filter('reach/partial/assets/components/single-quote-block', __NAMESPACE__ . '\\filterArgs');
